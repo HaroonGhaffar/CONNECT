f3bd7733958ca6dfe920b35fb6f14322ec264e04
Haroon Ghaffar <raoharoonghaffar786@gmail.com>
2026-06-20 18:53:37 +0500

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://connect-backend-1z2r.onrender.com",
+  baseURL: "https://connect-backend-1z2r.onrender.com/api",
 });
 
 export default API;
